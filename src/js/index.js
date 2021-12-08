cf145cf10a760d383339d1020b077f3cb2dd36fd
Oleksii Ostafiichuk <issimo333@gmail.com>
2021-12-08 15:49:32 +0200

    библиотеки

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,15 @@
 import '../css/main.css';
 import { fetchCards } from '../js/fetchCards';
 import markup from '../hbs/markup.hbs';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-import axios from 'axios';
-axios.get('/users').then(res => {
-  console.log(res.data);
-});
+const axios = require('axios').default;
+
+// import axios from 'axios';
+// axios.get('/users').then(res => {
+//   console.log(res.data);
+// });
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import simpleLightbox from 'simplelightbox';
