0e57961760d3e7c4e9d85eda7089af0eb6704f8e
Oleksii Ostafiichuk <issimo333@gmail.com>
2021-12-22 21:18:54 +0200

    почти готово

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,9 +1,32 @@
-import loadMore from "./index.js"
+import axios from "axios";
+import Notiflix from 'notiflix';
 
-async function fetchImages(search, page = 1) {
-   const response = await fetch(`https://pixabay.com/api/?key=24642073-b12941d2e5507c49f57c90e2e&q=${search}&image_type=photo&orientation=horizontal&safesearch=true&fields=webformatURL,largeImageURL,tags,likes,views,comments,downloads&per_page=40&page=${page}`)
-    const image = await response.json()
-    return image;
+export default class APIService {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+    }
+
+    async fetchHits(){
+    try { const response = await axios.get(`https://pixabay.com/api/?key=24781056-75ab6f95c382245b51e5e78bf&q=${this.searchQuery}&per_page=40&page=${this.page}&orientation=horizontal&safesearch=true&image_type=photo`)
+       this.page += 1;
+        if (response.data.hits.length === 0) {
+            return Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.');
+        }
+            return response.data;
+        }
+            catch (error) {   
+                console.log(error);
+            }
+    }
+    resetPage() {
+        this.page = 1;
+    }
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        searchQuery = newQuery;
+    }
 }
-
-export default { fetchImages };
--- src/js/index.js
+++ src/js/index.js
@@ -1,50 +1,80 @@
 import '../css/main.css';
-import { fetchImages } from '../js/fetchImages.js';
-import markup from '../hbs/markup.hbs';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-const axios = require('axios').default;
 
-const form = document.getElementById('search-form');
-const galleryImages = document.querySelector('.gallery');
+import APIService from './fetchImages';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import Notiflix from 'notiflix';
+
+
+const form = document.querySelector('.search-form');
 const loadMore = document.querySelector('.load-more');
-const input = document.querySelector('input')
-// console.log(input);
-let count = 1;
-let gallery = new SimpleLightbox('.gallery a');
+const galleryImages = document.querySelector('.gallery');
+const apiService = new APIService;
 
-loadMore.classList.add('hidden')
+form.addEventListener('submit', onSearch);
+loadMore.addEventListener('click', onLoadMore);
+loadMore.setAttribute(`disabled`, true);
+
+function onSearch(event) {
+  event.preventDefault();
+  
+  apiService.searchQuery = event.target.elements.searchQuery.value;
+  const inputV = apiService.searchQuery.trim();
+  if (inputV === '') {
+    return 
+  }
+  loadMore.setAttribute(`disabled`, true);
+
+  apiService.resetPage();
+  apiService.fetchHits()
+    .then(data => {
+       const imagesHits = data.totalHits;
+    Notiflix.Notify.success(`Hooray! We found ${imagesHits} images`);
+      galleryImages.innerHTML = '';
+      const hits = data.hits;
+      render(hits);
+      loadMore.removeAttribute(`disabled`, true);
+      
+    }); 
  
-async function formSubmit(event) {
-  event.preventDefault()
-  const image = await fetchImages(input.value)
-  count = 1
-  if (image.hits.length === 0) {
-        Notify.info("Sorry, there are no images matching your search query. Please try again.")
-    }
-  galleryImages.innerHTML = await image.hits.map(el => markup(el)).join('');
-  // const Counrties = value.map(el => countryListMarkup(el)).join('');
-
-gallery.on('show.simplelightbox', simpleLightbox);
-
-    Notify.success(`Hooray! We found ${image.totalHits} totalHits images.`)
-  loadMore.classList.remove('hidden');
 }
-// console.log(inputValue);
+function onLoadMore() {
+  apiService.fetchHits().then(el => {
+    render(el.hits)
+    lightbox.refresh()
+  });
+}
 
-// import axios from 'axios';
-// axios.get('/users').then(res => {
-//   console.log(res.data);
-// });
-
-// async function getUser() {
-//   try {
-//     const response = await axios.get('/user?ID=12345');
-//     console.log(response);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-form.addEventListener('submit', formSubmit)
+function render(data) {
+   
+  const markup = data
+        .map(({ largeImageURL, webformatURL, likes, views, comments, downloads, }) =>
+        {
+          return `
+  <a class="photo-card" href="${largeImageURL}">
+  <div class="img-wrapper">      
+  <img class="img-item" src="${webformatURL}" alt="#" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+      <span>${likes}</span>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    <span>${views}</span>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    <span>${comments}</span>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    <span>${downloads}</span>
+    </p>
+  </div>
+</div>
+</a>`;
+        }).join('');
+  galleryImages.insertAdjacentHTML('beforeend', markup);
+};
+const lightbox = new SimpleLightbox('.gallery a');
