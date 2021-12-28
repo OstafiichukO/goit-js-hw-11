b8ea847539ce835a582f92ff4ff282986fa37d9e
Oleksii Ostafiichuk <issimo333@gmail.com>
2021-12-28 22:21:50 +0200

    up

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,32 +1,37 @@
-import axios from "axios";
+import axios from 'axios';
 import Notiflix from 'notiflix';
 
 export default class APIService {
-    constructor() {
-        this.searchQuery = '';
-        this.page = 1;
-    }
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.per_page = 100;
+  }
 
-    async fetchHits(){
-    try { const response = await axios.get(`https://pixabay.com/api/?key=24781056-75ab6f95c382245b51e5e78bf&q=${this.searchQuery}&per_page=40&page=${this.page}&orientation=horizontal&safesearch=true&image_type=photo`)
-       this.page += 1;
-        if (response.data.hits.length === 0) {
-            return Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.');
-        }
-            return response.data;
-        }
-            catch (error) {   
-                console.log(error);
-            }
+  async fetchHits() {
+    try {
+      const response = await axios.get(
+        `https://pixabay.com/api/?key=25016497-6e844d2237e7dc6bdc93b7c9e&q=${this.searchQuery}&per_page=100&page=${this.page}&orientation=horizontal&safesearch=true&image_type=photo`,
+      );
+      this.page += 1;
+      if (response.data.hits.length === 0) {
+        return Notiflix.Notify.info(
+          'Sorry, there are no images matching your search query. Please try again.',
+        );
+      }
+      return response.data;
+    } catch (error) {
+      console.log(error);
     }
-    resetPage() {
-        this.page = 1;
-    }
-    get query() {
-        return this.searchQuery;
-    }
+  }
+  resetPage() {
+    this.page = 1;
+  }
+  get query() {
+    return this.searchQuery;
+  }
 
-    set query(newQuery) {
-        searchQuery = newQuery;
-    }
+  set query(newQuery) {
+    searchQuery = newQuery;
+  }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,60 +1,55 @@
 import '../css/main.css';
-
 import APIService from './fetchImages';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import Notiflix from 'notiflix';
-
 
 const form = document.querySelector('.search-form');
 const loadMore = document.querySelector('.load-more');
 const galleryImages = document.querySelector('.gallery');
-const apiService = new APIService;
+const apiService = new APIService();
 
 form.addEventListener('submit', onSearch);
 loadMore.addEventListener('click', onLoadMore);
+
 loadMore.setAttribute(`disabled`, true);
-// loadMore.classList.add('hidden')
+loadMore.classList.add('hidden');
 
 function onSearch(event) {
   event.preventDefault();
-  
   apiService.searchQuery = event.target.elements.searchQuery.value;
   const inputV = apiService.searchQuery.trim();
   if (inputV === '') {
-    return 
+    return;
   }
   loadMore.setAttribute(`disabled`, true);
   // loadMore.classList.add('hidden')
 
   apiService.resetPage();
-  apiService.fetchHits()
-    .then(data => {
-      const imagesHits = data.totalHits;
-      galleryImages.innerHTML = '';
-      const hits = data.hits;
-      render(hits);
-      lightbox.refresh()
-      Notiflix.Notify.success(`Hooray! We found ${imagesHits} images`);
-      loadMore.removeAttribute(`disabled`, true);
-      // loadMore.classList.remove('hidden')
-      
-    }); 
- 
+  apiService.fetchHits().then(data => {
+    const imagesHits = data.totalHits;
+    Notiflix.Notify.success(`Hooray! We found ${imagesHits} images`);
+    galleryImages.innerHTML = '';
+    const hits = data.hits;
+    render(hits);
+    lightbox.refresh();
+
+    loadMore.removeAttribute(`disabled`, true);
+    loadMore.classList.remove('hidden');
+  });
 }
+
 function onLoadMore() {
-  apiService.fetchHits().then(el => {
-    render(el.hits)
-    lightbox.refresh()
+  apiService.fetchHits().then(data => {
+    render(data.hits);
+    lightbox.refresh();
   });
 }
 
 function render(data) {
-   
   const markup = data
-        .map(({ largeImageURL, webformatURL, tags, likes, views, comments, downloads, }) =>
-        {
-          return `
+    .map(({ largeImageURL, webformatURL, tags, likes, views, comments, downloads }) => {
+      return `
   <a class='photo-card' href="${largeImageURL}">
   <div class="img-wrapper">
     <img src='${webformatURL}' alt='${tags}' loading='lazy' />
@@ -78,28 +73,9 @@ function render(data) {
     </p>
   </div>
 </a>`;
-        }).join('');
+    })
+    .join('');
   galleryImages.insertAdjacentHTML('beforeend', markup);
-};
+}
+
 const lightbox = new SimpleLightbox('.gallery a');
-
-// function lightBox() {
-//   const lightbox = new SimpleLightbox('.gallery a', {
-//     captions: true,
-//     overlay: true,
-//     preloading: true,
-//     alertErrorMessage: 'Изображение не найдено, будет загружено следующее изображение',
-//   });
-//   return lightBox;
-// }
-
-// const lightbox = event => {
-//     event.preventDefault();
-//     new SimpleLightbox('.gallery a',
-//         {
-//             captionsData: 'alt',
-//             captionDelay: 250
-//         }
-//     );
-// };
-// galleryImages.addEventListener('click', settingsSimpleLightbox);
